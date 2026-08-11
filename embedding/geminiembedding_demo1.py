7f7cd2948d26f0a1cc296a20bf00cec708573b29
kazrkb <kazrkb@gmail.com>
2026-08-11 11:25:33 +0600

    used gemini model embedding basics

---
+++ embedding/geminiembedding_demo1.py
@@ -0,0 +1,14 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2"
+)
+
+vector = embeddings.embed_query("What is the capital of Bangladesh?")
+
+print(len(vector),vector[:10])
+
